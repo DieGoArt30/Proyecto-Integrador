050b3ac0f435689cf869c57f8564294981ad9827
DieGoArt30 <149025522+DieGoArt30@users.noreply.github.com>
2023-11-10 21:06:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Crear un archivo README.md con la descripción del proyecto escrita por ti, esta será tu documentación del proyecto.
+
+# Crear el archivo main del proyecto.
+
+# Pedir el nombre del jugador por teclado.
+nombre_jugador = input("Nombre de Jugador: ")
+# Imprimir un mensaje de bienvenida con el nombre.
+print(f"Bienvenido al Laberinto ¡¡¡{nombre_jugador}!!!")
